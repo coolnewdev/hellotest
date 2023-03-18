4e105a7dbfff4eaa76c7ea94c183e8cc7530fc25
coolnewdev <128251964+coolnewdev@users.noreply.github.com>
2023-03-18 19:17:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-function fetchURL() {
-  var url = document.getElementById('url').value;
-  fetch(url)
-    .then(response => response.text())
-    .then(data => {
-      document.getElementById('result').innerHTML = data;
-    });
+function loadUrl() {
+  var input = document.getElementById("url-input").value;
+  var url = "https://cors-anywhere.herokuapp.com/" + input;
+
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", url, true);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      document.getElementById("response").innerHTML = xhr.responseText;
+    }
+  };
+  xhr.send();
 }
